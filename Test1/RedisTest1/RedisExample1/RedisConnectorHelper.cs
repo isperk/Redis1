2e28b5cc00c641eaa05215ae764871024cdc0af6
Jose Ignacio <jsperk@gmail.com>
2018-09-22 01:52:34 +0200

    Test1 Redis

---
+++ Test1/RedisTest1/RedisExample1/RedisConnectorHelper.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisExample1
+{
+    public class RedisConnectorHelper
+    {
+        static RedisConnectorHelper()
+        {
+            lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                var conn = ConnectionMultiplexer.Connect("localhost");
+                return conn;
+            });
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
---
+++ Test1/RedisTest1/RedisTest1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisTest1
+{
+    static class Program
+    {
+
+        [STAThread]
+        public static void Main()
+        {
+
+        }
+
+
+    }
+}
